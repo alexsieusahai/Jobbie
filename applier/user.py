863e42666a17620aeb90cdd92443266d173d3c87
itspax <alexsieu14@gmail.com>
2018-11-19 21:51:28 -0700

    removing user artifact

--- applier/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    """
-    The definition of a user.
-    """
-    def __init__(self, name, languages, buzzwords):
-        self.name = name
-        self.languages = languages
-        self.buzzwords = buzzwords
